da990a80e5e4a9634055656a3c9f19e4ec13915e
Timber-MacAir <icejm1020@163.com>
2024-07-12 14:13:51 +0800

    run vue project with electron

---
+++ out/main/main.js
@@ -0,0 +1,51 @@
+import { app, ipcMain, BrowserWindow, screen, shell } from "electron";
+import { join } from "path";
+import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import __cjs_mod__ from "node:module";
+const __filename = import.meta.filename;
+const __dirname = import.meta.dirname;
+const require2 = __cjs_mod__.createRequire(import.meta.url);
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    // set the size of the window
+    width: screen.getPrimaryDisplay().workAreaSize.width,
+    height: screen.getPrimaryDisplay().workAreaSize.height,
+    minWidth: 800,
+    minHeight: 450,
+    show: false,
+    autoHideMenuBar: true,
+    // ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, "../preload/index.mjs"),
+      sandbox: false
+    }
+  });
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url);
+    return { action: "deny" };
+  });
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+  } else {
+    mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
+  }
+}
+app.whenReady().then(() => {
+  electronApp.setAppUserModelId("com.electron");
+  app.on("browser-window-created", (_, window) => {
+    optimizer.watchWindowShortcuts(window);
+  });
+  ipcMain.on("ping", () => console.log("pong"));
+  createWindow();
+  app.on("activate", function() {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
